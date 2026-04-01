65020cf4da8d0c0541f9792eea57bdf4869a894b
Aimeniqbal2 <aimeniqbal2001@gmail.com>
2026-04-02 01:34:53 +0500

    fixing settings.py

--- erp_core/settings.py
+++ erp_core/settings.py
@@ -280,8 +280,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media Files (uploads, service videos, etc.)
 MEDIA_URL = '/media/'
